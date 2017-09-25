a4914e7a78a8875d09a51a7c0b19407c37c1773f
Petro <petro.petr@gmail.com>
2017-09-25 19:02:38 +0300

    sdfas

---
+++ Dev_Java_Spring_Boot/src/main/java/ua/com/yarema/model/request/OpenCloseRequest.java
@@ -0,0 +1,25 @@
+package ua.com.yarema.model.request;
+
+public class OpenCloseRequest {
+	
+	private Integer id;
+	
+	private String time;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+}
